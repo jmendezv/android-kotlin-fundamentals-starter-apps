9dc2c4b7114653b35e0ed738ea31a0f7a1a25f1e
Pep <pep.mendez@copernic.cat>
2021-11-12 19:59:13 +0100

    RecyclerView fundamentals:  Headers in RecyclerView complete simplified

--- TrackMySleepQuality-Starter/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
+++ TrackMySleepQuality-Starter/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
@@ -27,8 +27,8 @@ class SleepNightAdapter(val clickListener: SleepNightListener) :
     fun addHeaderAndSubmitList(list: List<SleepNight>?) {
         adapterScope.launch {
             val items = when (list) {
-                null -> listOf(DataItem.Header)
-                else -> listOf(DataItem.Header) + list.map { DataItem.SleepNightItem(it) }
+                null -> listOf(Header)
+                else -> listOf(Header) + list.map { SleepNightItem(it) }
             }
             withContext(Dispatchers.Main) {
                 submitList(items)
@@ -36,49 +36,6 @@ class SleepNightAdapter(val clickListener: SleepNightListener) :
         }
     }
 
-    /*
-     When notifyDataSetChanged() is called, the RecyclerView redraws the whole list,
-     not just the changed items. This marks the entire list as potentially invalid.
-     As a result, RecyclerView rebinds and redraws every item in the list,
-     including items that are not visible on screen. This is a lot of unnecessary work.
-     RecyclerView has a class called DiffUtil which is for calculating the differences
-     between two lists.
-     It uses an algorithm called Eugene W. Myers's difference algorithm to figure out
-     the minimum number of changes to make from the old list to produce the new list.
-     Once DiffUtil figures out what has changed, RecyclerView can use that information
-     to update only the items that were changed, added, removed, or moved, which is much
-     more efficient than redoing the entire list.
-     */
-/*    var data = listOf<SleepNight>()
-        set(value) {
-            field = value
-            notifyDataSetChanged()
-        }*/
-
-
-    /**
-     * Called when RecyclerView needs a new [SleepViewHolder] of the given type to represent
-     * an item.
-     *
-     *
-     * This new ViewHolder should be constructed with a new View that can represent the items
-     * of the given type. You can either create a new View manually or inflate it from an XML
-     * layout file.
-     *
-     *
-     * The new ViewHolder will be used to display items of the adapter using
-     * [.onBindViewHolder]. Since it will be re-used to display
-     * different items in the data set, it is a good idea to cache references to sub views of
-     * the View to avoid unnecessary [View.findViewById] calls.
-     *
-     * @param parent The ViewGroup into which the new View will be added after it is bound to
-     * an adapter position.
-     * @param viewType The view type of the new View.
-     *
-     * @return A new ViewHolder that holds a View of the given view type.
-     * @see .getItemViewType
-     * @see .onBindViewHolder
-     */
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         return when (viewType) {
             ITEM_VIEW_TYPE_HEADER -> TextViewHolder.from(parent)
@@ -87,32 +44,10 @@ class SleepNightAdapter(val clickListener: SleepNightListener) :
         }
     }
 
-
-    /**
-     * Called by RecyclerView to display the data at the specified position. This method should
-     * update the contents of the [SleepViewHolder.itemView] to reflect the item at the given
-     * position.
-     *
-     *
-     * Note that unlike [android.widget.ListView], RecyclerView will not call this method
-     * again if the position of the item changes in the data set unless the item itself is
-     * invalidated or the new position cannot be determined. For this reason, you should only
-     * use the `position` parameter while acquiring the related data item inside
-     * this method and should not keep a copy of it. If you need the position of an item later
-     * on (e.g. in a click listener), use [SleepViewHolder.getAdapterPosition] which will
-     * have the updated adapter position.
-     *
-     * Override [.onBindViewHolder] instead if Adapter can
-     * handle efficient partial bind.
-     *
-     * @param holder The ViewHolder which should be updated to represent the contents of the
-     * item at the given position in the data set.
-     * @param position The position of the item within the adapter's data set.
-     */
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         when (holder) {
             is SleepViewHolder -> {
-                val nightItem = getItem(position) as DataItem.SleepNightItem
+                val nightItem = getItem(position) as SleepNightItem
                 holder.bind(nightItem.sleepNight, clickListener)
             }
         }
@@ -120,8 +55,8 @@ class SleepNightAdapter(val clickListener: SleepNightListener) :
 
     override fun getItemViewType(position: Int): Int {
         return when (getItem(position)) {
-            is DataItem.Header -> ITEM_VIEW_TYPE_HEADER
-            is DataItem.SleepNightItem -> ITEM_VIEW_TYPE_ITEM
+            is Header -> ITEM_VIEW_TYPE_HEADER
+            is SleepNightItem -> ITEM_VIEW_TYPE_ITEM
         }
     }
 
@@ -135,29 +70,16 @@ class SleepNightAdapter(val clickListener: SleepNightListener) :
         }
     }
 
-    /**
-     * Returns the total number of items in the data set held by the adapter.
-     *
-     * @return The total number of items in this adapter.
-     *
-     * The best place to get information from one clicked item is in the ViewHolder object,
-     * since it represents one list item.
-     *
-     */
-//    override fun getItemCount() = data.size
-
     class SleepViewHolder private constructor(val binding: ListItemSleepNightBinding):
         RecyclerView.ViewHolder(binding.root) {
 
-        fun bind(
-            item: SleepNight,
-            clickListener: SleepNightListener
-        ) {
+        fun bind(item: SleepNight, clickListener: SleepNightListener) {
             binding.sleep = item
             binding.clickListener = clickListener
             // Usefull when using binding adapters in a RecyclerView
             binding.executePendingBindings()
         }
+
         companion object {
             fun from(parent: ViewGroup): SleepViewHolder {
                 val layoutInflater = LayoutInflater.from(parent.context)
@@ -170,9 +92,13 @@ class SleepNightAdapter(val clickListener: SleepNightListener) :
 }
 
 class SleepNightDiffCallback : DiffUtil.ItemCallback<DataItem>() {
+
+    /* Two items with equal id are the same */
     override fun areItemsTheSame(oldItem: DataItem, newItem: DataItem): Boolean {
         return oldItem.id == newItem.id
     }
+
+    /* Two items with the same properties values are the same */
     @SuppressLint("DiffUtilEquals")
     override fun areContentsTheSame(oldItem: DataItem, newItem: DataItem): Boolean {
         return oldItem == newItem
@@ -183,15 +109,12 @@ class SleepNightListener(val clickListener: (sleepId: Long) -> Unit) {
     fun onClick(night: SleepNight) = clickListener(night.nightId)
 }
 
-sealed class DataItem {
+sealed class DataItem { abstract val id: Long }
 
-    data class SleepNightItem(val sleepNight: SleepNight): DataItem() {
-        override val id = sleepNight.nightId
-    }
+data class SleepNightItem(val sleepNight: SleepNight): DataItem() {
+    override val id = sleepNight.nightId
+}
 
-    object Header: DataItem() {
-        override val id = Long.MIN_VALUE
-    }
-
-    abstract val id: Long
+object Header: DataItem() {
+    override val id = Long.MIN_VALUE
 }
