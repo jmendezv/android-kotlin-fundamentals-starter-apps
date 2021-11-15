bbe01169ac61a5d3b991c24e68f81d1c8f54f6ce
pep <jmendez1@xtec.cat>
2021-11-15 17:03:56 +0100

    MarsRealEstate-Starter first codelab 5th step done

--- MarsRealEstate-Starter/app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
+++ MarsRealEstate-Starter/app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
@@ -17,4 +17,36 @@
 
 package com.example.android.marsrealestate.network
 
+import com.squareup.moshi.Moshi
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+//import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.http.GET
+
 private const val BASE_URL = "https://android-kotlin-fun-mars-server.appspot.com/"
+
+private val moshi = Moshi.Builder()
+    .add(com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory())
+    .build()
+
+private val retrofit = Retrofit
+    .Builder()
+//    .addConverterFactory(ScalarsConverterFactory.create())
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
+    .baseUrl(BASE_URL)
+    .build()
+
+interface MarsApiService {
+    // GET defines the endpoint for the web service URL
+    @GET("realestate")
+//    fun getProperties(): Call<String>
+    fun getProperties(): Call<List<MarsProperty>>
+}
+
+// Singleton Retrofit object that implements MarsApiService
+object MarsApi {
+    val retrofitService: MarsApiService by lazy {
+        retrofit.create(MarsApiService::class.java)
+    }
+}
